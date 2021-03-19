dacd25e1fbf18a0927ab2610dc4811147d3a8a9c
aes89 <adrienne.e.smith@outlook.com>
2021-03-19 13:57:31 +1100

    Rm Old Files, Add CHaracter Class

---
+++ nursinghome/character_classes.rb
@@ -0,0 +1,14 @@
+class Characters 
+    attr_accessor :characters
+
+    def initialize()
+        @characters = [
+        {"name" => "Dorris", "weight" => 50, "age" => 90, "strenghts" =>  "stuff", "stats" => "write later"},
+        {"name" => "Enid", "weight" => 80, "age" => 80, "strenghts" =>  "stuff", "stats" => "write later"},
+        {"name" => "Henry", "weight" => 95, "age" => 85, "strenghts" =>  "stuff", "stats" => "write later"},
+        {"name" => "Soggybottom", "weight" => 60, "age" => 80, "strenghts" =>  "stuff", "stats" => "write later"}
+        ]
+    end
+end
+
+
---
+++ nursinghome/playme.rb
@@ -0,0 +1,10 @@
+require_relative "character_classes"
+
+# puts "welcome text about nursing home blah blah"
+# puts "What's your name?"
+# name = gets.chomp
+# p "Welcome #{name}!"
+# puts "Choose your character!"
+
+
+puts Characters.new.characters.length
