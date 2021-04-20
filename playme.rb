718ab8327d7f239b405870a6e1d19992b2d2c38d
aes89 <adrienne.e.smith@outlook.com>
2021-04-20 16:58:58 +1000

    Add: Character Preview and Confirm Input validation needed

--- playme.rb
+++ playme.rb
@@ -6,40 +6,58 @@ puts "What's your name?"
 name = gets.strip
 puts "Welcome #{name}!"
 puts "Choose your character!"
+
 puts "Who do you want to preview?"
 
+
+
 def preview_choices(input)
-    if input == "Dorris"
-        puts "Dorris stats"
-        select_character
-    elsif input == "Enid"
-        puts "Enid preview"
-        puts "Do you want to select Enid?"
+        if input == "Dorris"
+            puts "Dorris stats"
+            select_character(input)
+        elsif input == "Enid"
+            puts "Enid preview"
+            select_character(input)
 
-    elsif input == "Henry"
-        puts "Henry Preview"
-        puts "Do you want to select Henry?"
+        elsif input == "Henry"
+            puts "Henry Preview"
+            select_character(input)
 
-    elsif input == "Soggybottom"
-        puts "Soggybottom preview"
-        puts "Do you want to select Soggybottom?"
+        elsif input == "Soggybottom"
+            puts "Soggybottom preview"
+            select_character(input)
 
-    else 
-        puts "please enter a name"
-    end
+        else 
+            puts "Please select a gerry."
+        end
 end
 
 def select_character(preview)
-puts "Do you want to select #{preview}?"
-input = gets.chomp
-yes_no_confirm(input)
+    puts "Do you want to select #{preview}?"
+    input = gets.chomp
+    yes_no_confirm(input)
 end
 
 # refactor to loop over char names
 def character_preview
-puts "Dorris, Enid, Henry or Soggybottom?"
-preview = gets.chomp
-preview_choices(preview)
+    puts "Dorris, Enid, Henry or Soggybottom?"
+    preview = gets.chomp
+    preview_choices(preview)
+end
+
+def yes_no_confirm(input)
+    if input=="no"
+        character_choice = false
+        character_preview
+    elsif input == "yes"
+        character_choice = true
+    end
+end
+
+character_choice = false
+
+while character_choice = false do
+    character_preview
 end
 
 character_preview
